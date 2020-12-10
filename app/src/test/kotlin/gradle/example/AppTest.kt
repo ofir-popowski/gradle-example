5d6bf1cf8f7aba3f5feda0c6a851339435950e45
Ofir Popowski <ofirpopowski@hotmail.com>
2020-12-10 18:13:28 +0200

    I forgot the actual test because im stupid

--- app/src/test/kotlin/gradle/example/AppTest.kt
+++ app/src/test/kotlin/gradle/example/AppTest.kt
@@ -1,11 +1,13 @@
 package gradle.example
 
+import custom.test.SomeTestService
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
 class AppTest {
     @Test fun testAppHasAGreeting() {
         val classUnderTest = App()
+        SomeTestService().sayHello()
         assertNotNull(classUnderTest.greeting, "app should have a greeting")
     }
 }
